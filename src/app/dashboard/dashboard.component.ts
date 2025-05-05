544961b4181d8c91aca6703ed9cb093a57748b66
Nour <nourbahloul.192002@gmail.com>
2025-05-05 13:51:54 +0100

    modifier dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductsService } from 'app/services/products.service';
-import { ChartOptions } from 'chart.js';
+import { ChartDataset, ChartOptions } from 'chart.js';
 import * as Chartist from 'chartist';
 // Si tu utilises chart.js
 // import { ChartDataset } from 'chart.js';
@@ -19,10 +19,19 @@ export class DashboardComponent implements OnInit {
   chartOptions: ChartOptions = {};
   chartLabelsline: string[] = ["enfant", "homme", "femme"];
   chartDatapie: any[] = [
-    {
+    { 
+      label: '',
       data: []
     }
   ];
+  chartDatabar: ChartDataset[] = [
+    {
+      label: 'Nombre de produits par intervalle de prix',
+      data: [] // sera rempli dynamiquement
+    }
+  ];
+  
+  chartLabelsbar: string[] = ["0-50 DT", "51-100 DT", "101-200 DT", "201+ DT"];  
   chartLabelspie: string[] = ["enfant", "homme", "femme"];
 
   constructor(private PS: ProductsService) {}
@@ -47,5 +56,25 @@ export class DashboardComponent implements OnInit {
       ];
       this.chartLabelsline = this.ProdName;
     });
+    this.PS.getProducts().subscribe((products) => {
+      const counts = [0, 0, 0, 0]; // correspond aux 4 intervalles
+  
+      for (let product of products) {
+        const price = product.price;
+  
+        if (price <= 50) counts[0]++;
+        else if (price <= 100) counts[1]++;
+        else if (price <= 200) counts[2]++;
+        else counts[3]++;
+      }
+  
+      // Mise à jour des données du graphique
+      this.chartDatabar = [{
+        label: 'Nombre de produits par intervalle de prix',
+        data: counts,
+        backgroundColor: ['#4caf50', '#2196f3', '#ff9800', '#f44336']
+      }];
+    });
+  
   }
 }
